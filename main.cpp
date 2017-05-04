925d9c31b79bba2897a087701e13ca5f5ae9ea07
欧阳建华 <oyoung_2012@hotmail.com>
2017-05-04 14:47:36 +0800

    修改测试用main.cpp

--- main.cpp
+++ main.cpp
@@ -11,26 +11,34 @@ int main(int argc, char **argv)
 
     se::network::http::Client http;
 
-    se::network::http::Request req("https://api-cn.faceplusplus.com/facepp/v3/detect");//    req.header("Content-Type", "application/json;charset=UTF-8");
+//    se::network::http::Request req("https://api-cn.faceplusplus.com/facepp/v3/detect");//    req.header("Content-Type", "application/json;charset=UTF-8");
+    se::network::http::Request req("https://www.faceplusplus.com.cn/api/official/demo/facepp/v3/detect");//    req.header("Content-Type", "application/json;charset=UTF-8");
+
+    std::string body;
 //
-//    req.body("{ \"key\":\"f7b67526c4395f273abd6ea0fc8f2e20\", \"info\":\"吃饭啦\", \"loc\":\"紫金创业园\", \"userid\":\"123456\"}");
+//    req.header("Cache-Control", " no-cache");
+//    req.header("Content-Type", " multipart/form-data; boundary=-----WebKitFormBoundary7MA4YWxkTrZu0gW");
+//    body.append("-----WebKitFormBoundary7MA4YWxkTrZu0gW").append("\r\n");
+//    body.append("Content-Disposition: form-data; name=\"").append("image_url\"\r\n\r\n")
+//            .append("https://www.faceplusplus.com.cn/images/detection/group-case.jpg\r\n");
+//    body.append("-----WebKitFormBoundary7MA4YWxkTrZu0gW").append("\r\n");
+//    body.append("Content-Disposition: form-data; name=\"").append("return_attributes\"\r\n\r\n")
+//            .append("gender,age\r\n");
+//    body.append("-----WebKitFormBoundary7MA4YWxkTrZu0gW--");
 
-    req.header("Content-Type", "multipart/formdata");
 
-    req.query("api_key", "78MCSFFqMIN_LoPPB0-K5QadTq2a0ZQb");
-    req.query("api_secret", "SpowA-Z4s_7_kwtqPlRRoGNZTQ9uyUug");
-//    req.formdata("api_key", "78MCSFFqMIN_LoPPB0-K5QadTq2a0ZQb");
-//    req.formdata("api_secret", "SpowA-Z4s_7_kwtqPlRRoGNZTQ9uyUug");
-    se::network::http::Request::FormData fdata;
-    fdata.type = se::network::http::Request::FORM_NAME_FILE_FILENAME_CONTENTTYPE;
-    fdata.file = "timg.jpeg";
-    fdata.fileName = "timg.jpeg";
-    fdata.contentType = "image/jpeg";
-    req.formdata("image_file", fdata);
+    req.header("Content-Type", "multipart/form-data");
+
+    se::network::http::Request::FormData formData;
+    formData.type = se::network::http::Request::FORM_NAME_FILE;
+    formData.name = "image_file";
+    formData.file = "./timg.jpeg";
+    req.formdata(formData);
+
 
     auto res = http.post(req).response();
 
-    auto body = res.body();
+    body = res.body();
 
     std::cout << body << std::endl;
 
