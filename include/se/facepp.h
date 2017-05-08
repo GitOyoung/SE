4f024da2ea6f2ca499e05b59c29897b8a79a4b1b
欧阳建华 <oyoung_2012@hotmail.com>
2017-05-08 11:36:03 +0800

    增加se/facepp.h头文件

---
+++ include/se/facepp.h
@@ -0,0 +1,12 @@
+//
+// Created by 欧阳建华 on 2017/5/8.
+//
+
+#ifndef SE_FACEPP_H_H
+#define SE_FACEPP_H_H
+
+#include <se/facepp/faceplusplus.h>
+#include <se/facepp/faceinfo.h>
+#include <se/facepp/faceset.h>
+
+#endif //SE_FACEPP_H_H
---
+++ include/se/facepp.h.h
@@ -0,0 +1,8 @@
+//
+// Created by 欧阳建华 on 2017/5/8.
+//
+
+#ifndef SE_FACEPP_H_H
+#define SE_FACEPP_H_H
+
+#endif //SE_FACEPP_H_H
