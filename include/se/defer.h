897e9a3779f633d6b16d7ddd8926ef4c181c88fb
欧阳建华 <oyoung_2012@hotmail.com>
2017-08-23 09:35:50 +0800

    添加延迟调用的Defer类

---
+++ include/se/defer.h
@@ -0,0 +1,43 @@
+//
+// Created by 欧阳建华 on 2017/8/22.
+//
+
+#ifndef SE_DEFER_H
+#define SE_DEFER_H
+
+#include <queue>
+#include <functional>
+
+
+/**
+ * using like this:
+ *
+ * Defer defer;
+ *
+ * ...
+ *
+ * defer.exec([](){
+ * ...
+ * });
+ *
+ *
+ * ...
+ *
+ *
+ *
+ *
+ */
+
+namespace se {
+    typedef std::function<void()> DeferClosure;
+    typedef std::queue<DeferClosure> DeferClosureQueue;
+    class Defer {
+        Defer();
+        DeferClosure& exec(const DeferClosure& deferClosure);
+        ~Defer();
+    private:
+        DeferClosureQueue closureQueue;
+    };
+}
+
+#endif //SE_DEFER_H
---
+++ src/base/defer.cpp
@@ -0,0 +1,26 @@
+//
+// Created by 欧阳建华 on 2017/8/22.
+//
+
+#include <se/defer.h>
+
+
+namespace se {
+    Defer::Defer() {}
+
+    DeferClosure& Defer::exec(const DeferClosure &deferClosure) {
+        closureQueue.push(deferClosure);
+        return *this;
+    }
+
+    Defer::~Defer() {
+        while(!closureQueue.empty()) {
+            auto& closure = closureQueue.front();
+            closureQueue.pop();
+            if(closure) {
+                closure();
+            }
+
+        }
+    }
+}
